8ec3202bbe3f2ed60d5d6900c8fc1d6651e82873
adrianAtDvelop <Adrian.Steinbach@d-velop.com>
2020-05-27 10:23:32 +0200

    Implementation of numbers and a list

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
 print("Hello, World!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
