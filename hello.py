577b2dee9dd54848261c693de5ce0564bc92d831
adrianAtDvelop <Adrian.Steinbach@d-velop.com>
2020-05-27 11:02:48 +0200

    Update hello.py
    Imlpementation of some functions.

--- hello.py
+++ hello.py
@@ -19,4 +19,18 @@ print(type(z))
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
 
+def my_function():
+  print("Hello from a function")
 
+my_function()
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
